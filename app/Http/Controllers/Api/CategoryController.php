a27eea0f8039fbe79fea5287237a2c43df0ddcd0
Zaqueu Santos <zaqueulima1@gmail.com>
2021-09-07 17:53:24 -0300

    feat: create categories CRUD

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -8,28 +8,37 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+    private $rules = [
+        'name' => 'required|max:255',
+        'is_active' => 'boolean'
+    ];
+
     public function index()
     {
-        //
+        return Category::all();
     }
 
     public function store(Request $request)
     {
-        //
+        $this->validate($request, $this->rules);
+        return Category::create($request->all());
     }
 
-    public function show(Category $category)
+    public function show(Category $category): Category
     {
-        //
+        return $category;
     }
 
     public function update(Request $request, Category $category)
     {
-        //
+        $this->validate($request, $this->rules);
+        $category->update($request->all());
+        return $category;
     }
 
     public function destroy(Category $category)
     {
-        //
+        $category->delete();
+        return response()->noContent();
     }
 }
