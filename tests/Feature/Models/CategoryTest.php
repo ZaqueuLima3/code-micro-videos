854a6a525f743fc1bb77453bd69692912872a986
Zaqueu Santos <zaqueulima1@gmail.com>
2021-09-20 08:56:49 -0300

    test: create Category tests

--- tests/Feature/Models/CategoryTest.php
+++ tests/Feature/Models/CategoryTest.php
@@ -1,11 +1,72 @@
 <?php
 
-namespace Tests\Models;
+namespace Tests\Feature\Models;
 
 use App\Models\Category;
-use PHPUnit\Framework\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
 
 class CategoryTest extends TestCase
 {
+    use DatabaseMigrations;
 
+    public function testCategoryList()
+    {
+        factory(Category::class, 1)->create();
+        $categories = Category::all();
+        $this->assertCount(1, $categories);
+        $categoriesKeys = array_keys($categories->first()->getAttributes());
+        $this->assertEqualsCanonicalizing([
+            'id', 'name', 'description', 'created_at', 'updated_at', 'deleted_at', 'is_active'
+        ], $categoriesKeys);
+    }
+
+    public function testCategoryCreate()
+    {
+        $category = Category::create([
+            'name' => 'test1'
+        ]);
+        $category->refresh();
+        $this->assertEquals('test1', $category->name);
+        $this->assertNull($category->description);
+        $this->assertTrue((bool)$category->is_active);
+        $category = Category::create([
+            'name' => 'test1',
+            'description' => null
+        ]);
+        $this->assertNull($category->description);
+        $category = Category::create([
+            'name' => 'test1',
+            'description' => 'test_description'
+        ]);
+        $this->assertEquals('test_description', $category->description);
+        $category = Category::create([
+            'name' => 'test1',
+            'is_active' => false
+        ]);
+        $this->assertFalse($category->is_active);
+        $category = Category::create([
+            'name' => 'test1',
+            'is_active' => true
+        ]);
+        $this->assertTrue($category->is_active);
+    }
+
+    public function testCategoryUpdate()
+    {
+        /** @yar Category $category */
+        $category = factory(Category::class)->create([
+            'description' => 'test_description',
+            'is_active' => false
+        ])->first();
+        $data = [
+            'name' => 'test_name_update',
+            'description' => 'test_description_update',
+            'is_active' => true
+        ];
+        $category->update($data);
+        foreach ($data as $key => $value) {
+            $this->assertEquals($value, $category->{$key});
+        }
+    }
 }
--- tests/Unit/Models/CategoryTest.php
+++ tests/Unit/Models/CategoryTest.php
@@ -5,23 +5,23 @@ namespace Tests\Unit\Models;
 use App\Models\Category;
 use App\Models\Traits\Uuid;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
 class CategoryTest extends TestCase
 {
-    use DatabaseMigrations;
+    private $category;
 
-    private function makeSut(): Category
+    protected function setUp(): void
     {
-        return new Category();
+        parent::setUp();
+        $this->category = new Category();
+
     }
 
     public function testFillableAttributeShouldHaveNameDescriptionAndIs_active()
     {
-        $category = $this->makeSut();
         $expected = ['name', 'description', 'is_active'];
-        $this->assertEquals($expected, $category->getFillable());
+        $this->assertEquals($expected, $this->category->getFillable());
     }
 
     public function testIfUseCorrectTraits()
@@ -36,24 +36,21 @@ class CategoryTest extends TestCase
 
     public function testCastsAttributeShouldHaveIdToString()
     {
-        $category = $this->makeSut();
-        $casts = ['id' => 'string'];
-        $this->assertEquals($casts, $category->getCasts());
+        $casts = ['id' => 'string', 'is_active' => 'boolean'];
+        $this->assertEquals($casts, $this->category->getCasts());
     }
 
     public function testIdIsNotAutoIncrement()
     {
-        $category = $this->makeSut();
-        $this->assertFalse($category->incrementing);
+        $this->assertFalse($this->category->incrementing);
     }
 
     public function testDateAttributeShouldHaveIdToString()
     {
-        $category = $this->makeSut();
         $expected = ['deleted_at', 'updated_at', 'created_at'];
         foreach ($expected as $date) {
-            $this->assertContains($date, $category->getDates());
+            $this->assertContains($date, $this->category->getDates());
         }
-        $this->assertCount(count($expected), $category->getDates());
+        $this->assertCount(count($expected), $this->category->getDates());
     }
 }
